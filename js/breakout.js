734493fbd0e2e5737fb1e4254d3aa1e7f4df5405
LindseyLovejoy <143131172+LindseyLovejoy@users.noreply.github.com>
2024-03-22 21:21:16 +0000

    Fri, Mar 22, 2024, 2:21 PM -07:00

---
+++ js/breakout.js
@@ -0,0 +1,34 @@
+btn = document.querySelector('.rules-btn')
+closeBtn = document.querySelector('.close-btn')
+rules = document.querySelector('.rules')
+canvas = documant.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+//Create ball's properties
+ball = {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+}
+
+//draw ball on canvas
+function drawBall() {
+ctx.beginPath()
+ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+ctx.stroke()
+}
+
+drawBall()
+
+
+//rules open & close event handelers
+btn.addEventListener('click', () => {
+    rules.classList.add('show');
+})
+
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show');
+})
