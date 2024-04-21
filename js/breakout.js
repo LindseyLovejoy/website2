0d302d1b4f928e9f88e23ba6f0227bfb3e431529
LindseyLovejoy <143131172+LindseyLovejoy@users.noreply.github.com>
2024-04-21 23:06:20 +0000

    Sun, Apr 21, 2024, 4:06 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -90,6 +90,7 @@ function drawPaddle() {
 
     // Draw everything
     function draw() {
+        ctx.clearRect(0,0,canvas.width, canvas.height)
         drawBall()
         drawPaddle()
         drawScore()
